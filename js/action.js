26a7c253d22b7c8612747695ad3825798a70f9fd
Eder L. Machado <eder.lourenco.machado@gmail.com>
2024-03-11 15:27:02 -0400

    Fixing - Image Card Border 	modified:   css/styles.css To fix image card border we did add the property below: border-top-left-radius: 5px; border-top-right-radius: 5px;

--- js/action.js
+++ js/action.js
@@ -21,7 +21,7 @@ for (let count = 1; count < totalPhotos; count++) {
   a.setAttribute("data-lightbox", "photos");
   img.setAttribute("class", "img-fluid");
   img.setAttribute("src", "images/galeria/foto (" + count + ").jpeg");
-  img.setAttribute("alt", "foto ("+ count +")");
+  img.setAttribute("alt", "foto (" + count + ")");
 
 
   a.appendChild(img);
@@ -60,5 +60,51 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
+/* captcha */
 
+// $(document).ready( ()=>{
+//   alert("Now I am working as I supposed to!!!");
+//   generateCaptcha();
+//   // Define the function generateCaptcha()
+//   function generateCaptcha() {
+//     // Get the canvas element with ID captcha and create an instance of its canvas rendering context
+//     var a = $("#captcha")[0],
+//         b = a.getContext("2d");
+//       // Clear the canvas
+//       b.clearRect(0, 0, a.width, a.height);
+//       // Define the string of characters that can be included in the captcha
+//       var f = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+//           e = "",
+//           g = -45,
+//           h = 45,
+//           i = h - g,
+//           j = 20,
+//           k = 30,
+//           l = k - j;
+//       // Generate each character of the captcha
+//       for (var m = 0; m < 6; m++) {
+//         // Select random letter from the pool to be part of the captcha
+//         var n = f.charAt(Math.floor(Math.random() * f.length));
+//         e += n;
+        
+//         // Set up the text formatting
+//         b.font = j + Math.random() * 10 + "px Arial";
+//         b.textAlign = "center";
+//         b.textBaseline = "middle";
+        
+//         // Set the color of the text
+//         b.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+    
+//         // Add the character to the canvas
+//         var o = g + Math.random() * i;
+//         b.translate(20 + m * 30, a.height / 2);
+//         b.rotate(o * Math.PI / 180);
+//         b.fillText(n, 0, 0);
+//         b.rotate(-1 * o * Math.PI / 180);
+//         b.translate(-(20 + m * 30), -1 * a.height / 2);
+//       }
+//       // Set the captcha code in session storage
+//       sessionStorage.setItem("captchaCode", e);
+//   }
+// });
 
